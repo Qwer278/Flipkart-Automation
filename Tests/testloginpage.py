ac089bb10e1d92f4b31f55e1eb17556ac23242b1
Ayush Jindal(Jindalayush987@gmal.com) <94276821+Qwer278@users.noreply.github.com>
2022-11-07 11:06:16 +0530

    final

--- Tests/testloginpage.py
+++ Tests/testloginpage.py
@@ -8,24 +8,24 @@ from Utilities.LoginUtility import *
 
 @pytest.mark.usefixtures("initiate_driver")
 class TestLoginPage:
-    def test_login_page_using_correct_phone_and_correct_password(self):
-        Find = Util()
-        LoginUtility().perform_login_with(username, password)
-        assert Find.find(profile_login).is_displayed(), 'Working for correct phone and correct password'
-
-    def test_login_using_wrong_phone_and_correct_pass(self, initiate_driver):
-        Find = Util()
-        LoginUtility().perform_login_with(invalid_username, password)
-        Find.wait_for(incorrect_statement_password)
-        incorrect_statement = Find.find(incorrect_statement_password)
-        assert incorrect_statement.is_displayed(), 'Working for wrong phone and correct password'
-
-    def test_login_using_wrong_pass_correct_phone(self):
-        Find = Util()
-        LoginUtility().perform_login_with(username, invalid_password)
-        Find.wait_for(incorrect_statement_password)
-        incorrect_statement = Find.find(incorrect_statement_password)
-        assert incorrect_statement.is_displayed(), 'Working for correct phone and wrong password'
+    # def test_login_page_using_correct_phone_and_correct_password(self):
+    #     Find = Util()
+    #     LoginUtility().perform_login_with(username, password)
+    #     assert Find.find(profile_login).is_displayed(), 'Working for correct phone and correct password'
+    #
+    # def test_login_using_wrong_phone_and_correct_pass(self, initiate_driver):
+    #     Find = Util()
+    #     LoginUtility().perform_login_with(invalid_username, password)
+    #     Find.wait_for(incorrect_statement_password)
+    #     incorrect_statement = Find.find(incorrect_statement_password)
+    #     assert incorrect_statement.is_displayed(), 'Working for wrong phone and correct password'
+    #
+    # def test_login_using_wrong_pass_correct_phone(self):
+    #     Find = Util()
+    #     LoginUtility().perform_login_with(username, invalid_password)
+    #     Find.wait_for(incorrect_statement_password)
+    #     incorrect_statement = Find.find(incorrect_statement_password)
+    #     assert incorrect_statement.is_displayed(), 'Working for correct phone and wrong password'
 
     def test_login_using_wrong_pass_wrong_phone(self):
         Find = Util()
